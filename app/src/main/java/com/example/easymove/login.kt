f813e5b852e1f28669a90ab42799fa4b556ea3c6
Michelangelo <mikimarconi2001@gmail.com>
2023-04-26 18:04:47 +0200

    modifiche

--- app/src/main/java/com/example/easymove/login.kt
+++ app/src/main/java/com/example/easymove/login.kt
@@ -8,27 +8,36 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 
-class login : AppCompatActivity(){
+class login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
 
-        val loginbtn=findViewById<Button>(R.id.login)
-        val user= findViewById<EditText>(R.id.Email)
-        val pass= findViewById<EditText>(R.id.Password)
+        val loginbtn = findViewById<Button>(R.id.login)
+        val user = findViewById<EditText>(R.id.Email)
+        val pass = findViewById<EditText>(R.id.Password)
 
         loginbtn.setOnClickListener {
 
-            if(user.getText().toString().equals("admin") && pass.getText().toString().equals("pass")){
+            if (!(user.getText().toString().isEmpty()) && !(pass.getText().toString().isEmpty())) {
 
-                val Intent = Intent(this, MainActivity::class.java)
-                startActivity(Intent)
+                if (user.getText().toString().equals("admin") && pass.getText().toString()
+                        .equals("pass")
+                ) {
 
+                    val Intent = Intent(this, MainActivity::class.java)
+                    startActivity(Intent)
+
+                } else {
+
+                    Toast.makeText(this, "Username o Password errati", Toast.LENGTH_SHORT).show()
+                }
             }
             else{
-
                 Toast.makeText(this, "Username o Password non inseriti", Toast.LENGTH_SHORT).show()
             }
-            }
+
         }
+
     }
+}
