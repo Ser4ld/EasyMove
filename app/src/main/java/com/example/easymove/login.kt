a4569417d8f97138f88d3bc03ed0c6c9096f783d
Michelangelo <mikimarconi2001@gmail.com>
2023-04-26 16:23:45 +0200

    primo login

--- app/src/main/java/com/example/easymove/login.kt
+++ app/src/main/java/com/example/easymove/login.kt
@@ -1,4 +1,12 @@
 package com.example.easymove
 
-class login {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class login : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+    }
+
 }
