ba47dc1a6cc7d71f1fe717af40380a3d3740a132
Ser4ld <S1097973@studenti.univpm.it>
2023-04-28 23:18:24 +0200

    implementazione nuova animazione

--- app/src/main/java/com/example/easymove/firstpage.kt
+++ app/src/main/java/com/example/easymove/firstpage.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.cardview.widget.CardView
 import com.google.android.material.card.MaterialCardView
 
@@ -19,15 +20,21 @@ class firstpage : AppCompatActivity() {
         val signupButton = findViewById<Button>(R.id.signup_button_index)
         val logo = findViewById<ImageView>(R.id.EasyMoveLogo)
         val cardview = findViewById<MaterialCardView>(R.id.cardview_index)
+        val text_header = findViewById<TextView>(R.id.textView_header)
+        val text_sub_header = findViewById<TextView>(R.id.textView_sub_header)
+
 
         val animation_1 = AnimationUtils.loadAnimation(this, R.anim.animation_1)
         val animation_2 = AnimationUtils.loadAnimation(this, R.anim.animation_2)
         val animation_3 = AnimationUtils.loadAnimation(this, R.anim.animation_3)
+        val animation_4 = AnimationUtils.loadAnimation(this, R.anim.animation_4)
 
         logo.startAnimation(animation_1)
         cardview.startAnimation(animation_2)
         loginButton.startAnimation(animation_3)
         signupButton.startAnimation(animation_3)
+        text_header.startAnimation(animation_4)
+        text_sub_header.startAnimation(animation_4)
 
         loginButton.setOnClickListener{
             val intentLogin = Intent(this, login::class.java)
