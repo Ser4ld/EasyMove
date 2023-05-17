f6c1fd8819af645fa13ce4a3b1c01b50763a5564
DavideTicchiarelli <s1098361@studenti.univpm.it>
2023-05-17 18:46:34 +0200

    zoom mappa

--- app/src/main/java/com/example/easymove/home/HomeFragment.kt
+++ app/src/main/java/com/example/easymove/home/HomeFragment.kt
@@ -5,16 +5,18 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.easymove.R
 import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.MapView
-import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.MapsInitializer
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
 class HomeFragment : Fragment(), OnMapReadyCallback {
+
     private lateinit var mapView: MapView
-    private lateinit var googleMap: GoogleMap
+    private var googleMap: GoogleMap? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,7 +31,7 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
         mapView.onCreate(savedInstanceState)
         mapView.onResume()
         try {
-            MapsInitializer.initialize(activity?.applicationContext)
+            MapsInitializer.initialize(requireActivity().applicationContext)
         } catch (e: Exception) {
             e.printStackTrace()
         }
@@ -40,22 +42,28 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
         return view
     }
 
-    override fun onMapReady(map: GoogleMap?) {
-        map?.let {
-            googleMap = it
+    override fun onMapReady(map: GoogleMap) {
+        googleMap = map
 
-            // Personalizza la mappa come desideri
-            // Esempio: Imposta il tipo di mappa
-            googleMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+        // Personalizza la mappa come desideri
+        // Esempio: Imposta il tipo di mappa
+        googleMap?.mapType = GoogleMap.MAP_TYPE_NORMAL
 
-            // Esegui altre operazioni sulla mappa
-            // ...
+        // Esegui altre operazioni sulla mappa
+        // ...
 
-            val startPoint = LatLng(43.58685879237719, 13.516592134589143)
-            val endPoint = LatLng(42.86820613903017, 13.921166498128718)
-            googleMap.addMarker(MarkerOptions().position(startPoint).title("Punto di partenza"))
-            googleMap.addMarker(MarkerOptions().position(endPoint).title("Punto di arrivo"))
-        }
+        val startPoint = LatLng(43.58685879237719, 13.516592134589143)
+        val endPoint = LatLng(42.86820613903017, 13.921166498128718)
+        googleMap?.addMarker(MarkerOptions().position(startPoint).title("Punto di partenza"))
+        googleMap?.addMarker(MarkerOptions().position(endPoint).title("Punto di arrivo"))
+
+        // Centra la mappa sulla posizione desiderata
+        val cameraPosition = CameraPosition.Builder()
+            .target(startPoint) // Posizione desiderata
+            .zoom(12f) // Livello di zoom desiderato
+            .build()
+
+        googleMap?.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
     }
 
     override fun onResume() {
