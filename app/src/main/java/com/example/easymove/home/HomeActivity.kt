8ed949509910c74fa4b8cfd84975584dff5028a7
Michelangelo <mikimarconi2001@gmail.com>
2023-08-10 19:00:26 +0200

    modifiche

--- app/src/main/java/com/example/easymove/home/HomeActivity.kt
+++ app/src/main/java/com/example/easymove/home/HomeActivity.kt
@@ -7,56 +7,79 @@ import com.example.easymove.R
 import com.example.easymove.databinding.HomeBinding
 import com.example.easymove.profilo.ProfileFragment
 import android.content.Intent
+import android.util.Log
 import android.view.View
 import com.example.easymove.CreaAnnuncioActivity
-
+import com.example.easymove.databinding.FragmentProfileBinding
+import com.example.easymove.profilo.ProfileViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: HomeBinding
-    private var tipoutente: String = "guidatore" //da cambiare e implementare bene quando sistemiamo pattern mvvm
+    private lateinit var firestore: FirebaseFirestore
+    private val user = FirebaseAuth.getInstance().currentUser
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.home)
-
-        binding=HomeBinding.inflate(layoutInflater)
+        binding = HomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        firestore = FirebaseFirestore.getInstance()
+
         replaceFragment(HomeFragment())
 
         binding.bottomAppBar.setBackgroundColor(resources.getColor(R.color.white))
         binding.bottomNavigationView.background = null
 
-        binding.addItem.setOnClickListener{
+        binding.addItem.setOnClickListener {
             intentAnnuncio()
         }
 
-        binding.bottomNavigationView.setOnItemSelectedListener {
-
-            /*quando viene cliccato un pulsante della bottomNavigationView, si effettua il controllo dell'id del pulsante e si passa il fragment corrispondente alla funzione ReplaceFragment*/
-            when(it.itemId){
+        binding.bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
                 R.id.homeItem -> replaceFragment(HomeFragment())
                 R.id.profileItem -> replaceFragment(ProfileFragment())
             }
             true
         }
 
-
-        if (tipoutente == "guidatore") { //da sistemare bene quando sistemiamo pattern mvvm
-            binding.addItem.visibility = View.VISIBLE
-        } else {
-            binding.addItem.visibility = View.GONE
-        }
-
+        // Qui chiamiamo la funzione per ottenere il valore di "tipoutente"
+        fetchAndSetTipoutente()
+    }
+    fun getUserId(): String {
+        val userId = user?.uid
+        return userId.toString()
     }
 
-    private fun intentAnnuncio(){
+    private fun fetchAndSetTipoutente() {
+        val userId = getUserId() // Sostituisci con l'ID dell'utente corrente
+
+        firestore.collection("users").document(userId)
+            .get()
+            .addOnSuccessListener { documentSnapshot ->
+                if (documentSnapshot.exists()) {
+                    val tipoutente = documentSnapshot.getString("tipoutente")
+                    if (tipoutente == "guidatore") {
+                        binding.addItem.visibility = View.VISIBLE
+                    } else {
+                        binding.addItem.visibility = View.GONE
+                    }
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.e("FirestoreError", "Errore durante la lettura dal Firestore", exception)
+            }
+    }
+
+    private fun intentAnnuncio() {
         val intent = Intent(this, CreaAnnuncioActivity::class.java)
         startActivity(intent)
     }
-    /*funziona replaceFragment viene utilizzato per visualizzare un fragment*/
-    private fun replaceFragment(fragment: Fragment){
-        val fragmentManager=supportFragmentManager
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.frameLayout, fragment)
         fragmentTransaction.commit()
@@ -66,6 +89,4 @@ class HomeActivity : AppCompatActivity() {
         // Non fare nulla quando viene premuto il pulsante "Indietro"
         // in modo che l'utente non possa tornare alla schermata di login
     }
-
 }
-
