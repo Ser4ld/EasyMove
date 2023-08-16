e186678d030d3b599516c5c68b7d1bbe17896459
DavideTicchiarelli <s1098361@studenti.univpm.it>
2023-08-16 21:28:38 +0200

    tolti i finish

--- app/src/main/java/com/example/easymove/ViewModel/SignupViewModel.kt
+++ app/src/main/java/com/example/easymove/ViewModel/SignupViewModel.kt
@@ -6,6 +6,7 @@ import com.example.easymove.model.User
 
 class SignupViewModel(private val userModel: User) : ViewModel() {
 
+
     fun signUp(email: String, password: String, repeatPassword: String, nome: String, cognome: String, tipoutente: String, callback: (Boolean, String?) -> Unit) {
         if (email.isNotEmpty() && password.isNotEmpty() && repeatPassword.isNotEmpty() && nome.isNotEmpty() && cognome.isNotEmpty()) {
             if (password == repeatPassword) {
