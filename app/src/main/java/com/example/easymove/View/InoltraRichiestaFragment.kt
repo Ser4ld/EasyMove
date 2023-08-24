173e9b59c031ec5cec8308827bd82fad2466aaef
Ser4ld <S1097973@studenti.univpm.it>
2023-08-24 15:54:58 +0200

    Selezione data InoltraRichiestaFragment.kt

--- app/src/main/java/com/example/easymove/View/InoltraRichiestaFragment.kt
+++ app/src/main/java/com/example/easymove/View/InoltraRichiestaFragment.kt
@@ -1,5 +1,6 @@
 package com.example.easymove.View
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +8,8 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.easymove.R
 import com.example.easymove.databinding.FragmentInoltraRichiestaBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class InoltraRichiestaFragment : Fragment() {
 
@@ -26,9 +29,15 @@ class InoltraRichiestaFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         binding.floatingActionButton.setOnClickListener {
             parentFragmentManager.popBackStack()
         }
+
+        binding.textData.setOnClickListener {
+            showDatePicker()
+        }
+
         val arguments = arguments
         if (arguments != null) {
             val modello = arguments.getString("modello")
@@ -41,5 +50,25 @@ class InoltraRichiestaFragment : Fragment() {
         }
     }
 
+    private fun showDatePicker() {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
 
+        val datePickerDialog = DatePickerDialog(
+            requireContext(),
+            { _, selectedYear, selectedMonth, selectedDay ->
+                val selectedDate = Calendar.getInstance()
+                selectedDate.set(selectedYear, selectedMonth, selectedDay)
+                val formattedDate = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(selectedDate.time)
+                binding.textData.setText(formattedDate)
+            },
+            year,
+            month,
+            day
+        )
+
+        datePickerDialog.show()
+    }
 }
