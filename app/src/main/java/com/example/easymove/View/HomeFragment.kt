ab90ef398ac0b9cc822a3f732f03ed111bb6a41a
Ser4ld <S1097973@studenti.univpm.it>
2023-08-27 13:53:15 +0200

    Settata mappa sull'HomeFragment

--- app/src/main/java/com/example/easymove/View/HomeFragment.kt
+++ app/src/main/java/com/example/easymove/View/HomeFragment.kt
@@ -2,6 +2,7 @@ package com.example.easymove.View
 
 import android.app.Activity
 import android.os.Bundle
+import android.provider.Settings
 import android.text.Editable
 import android.util.Log
 import android.view.LayoutInflater
@@ -35,6 +36,7 @@ import java.net.URL
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import com.example.easymove.ViewModel.HomeViewModel
+import com.example.easymove.ViewModel.MapViewModel
 import com.example.easymove.ViewModel.UserViewModel
 import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.PolylineOptions
@@ -49,15 +51,18 @@ class HomeFragment : Fragment() , OnMapReadyCallback {
 
     private lateinit var binding: FragmentHomeBinding
     private lateinit var homeViewModel: HomeViewModel
+    private lateinit var mapViewModel: MapViewModel
 
     private lateinit var mMap: GoogleMap
 
-    private var focusOriginBool: Boolean= false
-    private var focusDestinationBool: Boolean= false
+
+    private var focusOriginBool: Boolean = false
 
 
     private var originData: MapData = MapData()
     private var destinationData: MapData = MapData()
+    private lateinit var distance: String
+    private lateinit var timeTravel: String
 
 
     private val startAutocomplete =
@@ -66,14 +71,45 @@ class HomeFragment : Fragment() , OnMapReadyCallback {
                 val intent = result.data
                 if (intent != null) {
                     val place = Autocomplete.getPlaceFromIntent(intent)
-                    getAddressDetails(place)
+                    mapViewModel.getAddressDetails(
+                        place,
+                        originData,
+                        destinationData,
+                        focusOriginBool
+                    )
+
+                    if (focusOriginBool) {
+                        binding.editTextOrigin.text =
+                            Editable.Factory.getInstance().newEditable(originData.address)
+                    } else {
+                        binding.editTextDestination.text =
+                            Editable.Factory.getInstance().newEditable(destinationData.address)
+                    }
 
                     Log.i("ProvaOrigine", "$originData")
                     Log.i("ProvaDestinazione", "$destinationData")
 
-                    homeViewModel.checkFormEditTexts(binding.editTextOrigin, binding.editTextDestination) { isValid, errorMessage ->
+                    homeViewModel.checkFormEditTexts(
+                        binding.editTextOrigin,
+                        binding.editTextDestination
+                    ) { isValid, errorMessage ->
                         if (isValid) {
-                            HttpRequestDirections()
+                            mapViewModel.HttpRequestDirections(
+                                viewModelScope = viewLifecycleOwner.lifecycleScope,
+                                originData,
+                                destinationData,
+                                getString(R.string.map_api_key)
+                            ) { JSONResponse ->
+                                val (parsedDistance, parsedTimeTravel) = mapViewModel.parseDistanceTimeFromJson(JSONResponse)
+
+                                distance = parsedDistance
+                                timeTravel = parsedTimeTravel
+                                Log.d("prova distanza", distance)
+                                Log.d("prova tempo", timeTravel)
+                                drawRouteOnMap(JSONResponse)
+
+                            }
+
                         } else {
                             if (errorMessage != null) {
                                 Log.e("Errore", errorMessage)
@@ -101,12 +137,18 @@ class HomeFragment : Fragment() , OnMapReadyCallback {
         super.onViewCreated(view, savedInstanceState)
 
         homeViewModel = ViewModelProvider(requireActivity()).get(HomeViewModel::class.java)
+        mapViewModel = ViewModelProvider(requireActivity()).get(MapViewModel::class.java)
 
         // Inizializza l'SDK
-        Places.initialize(requireContext(), getString(R.string.map_api_key) )
+        Places.initialize(requireContext(), getString(R.string.map_api_key))
 
         // Definisce i campi che devono essere restituiti in seguito alla selezione
-        val fields = listOf( Place.Field.NAME, Place.Field.ADDRESS,  Place.Field.ADDRESS_COMPONENTS,Place.Field.LAT_LNG)
+        val fields = listOf(
+            Place.Field.NAME,
+            Place.Field.ADDRESS,
+            Place.Field.ADDRESS_COMPONENTS,
+            Place.Field.LAT_LNG
+        )
 
         // Avvia l'intent di autocompletamento
         val intent = Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, fields)
@@ -117,17 +159,18 @@ class HomeFragment : Fragment() , OnMapReadyCallback {
         // Controlla il focus del primo EditText
         binding.editTextOrigin.setOnFocusChangeListener { _, hasFocus ->
             if (hasFocus) {
-                focusOriginBool=true
+                focusOriginBool = true
                 startAutocomplete.launch(intent) // Lancia l'autocomplete
                 binding.editTextOrigin.clearFocus() // Toglie il focus
                 binding.editTextOrigin.isCursorVisible = false // Toglie il cursore
+
             }
         }
 
         // Controlla il focus del secondo EditText
         binding.editTextDestination.setOnFocusChangeListener { _, hasFocus ->
             if (hasFocus) {
-                focusDestinationBool=true
+                focusOriginBool = false
                 startAutocomplete.launch(intent) // Lancia l'autocomplete
                 binding.editTextDestination.clearFocus() // Toglie il focus
                 binding.editTextDestination.isCursorVisible = false // Toglie il cursore
@@ -150,146 +193,29 @@ class HomeFragment : Fragment() , OnMapReadyCallback {
         val montedagoAncona = LatLng(43.608973, 13.512643)
 
         // Aggiunge il marker sulle coordinate passate in ingresso
-        mMap.addMarker(MarkerOptions()
-            .position(montedagoAncona)
-            .title("Polo Montedago, Ancona"))
+        mMap.addMarker(
+            MarkerOptions()
+                .position(montedagoAncona)
+                .title("Polo Montedago, Ancona")
+        )
 
         val zoomLevel = 12.0f // Imposta il livello di zoom
 
         // Definisce la posizione e lo zoom della camera
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(montedagoAncona, zoomLevel))    }
-
-    private fun getAddressDetails(place: Place){
-
-        // Estrae dalla variabile place restituita dall'autocomplete addressComponents
-        val addressComponents = place.addressComponents.asList()
-
-        // Se il focus è sull'Origin editext riempie i campi dell'oggetto originData altrimenti di destinationData
-        if(focusOriginBool){
-            originData.address =place.address
-
-            binding.editTextOrigin.text=Editable.Factory.getInstance().newEditable(originData.address)
-
-            originData.latitude=place.latLng.latitude.toString()
-            originData.longitude=place.latLng.longitude.toString()
-
-            for (component in addressComponents) {
-                val types = component.types
-                val name = component.name
-
-                when {
-                    "locality" in types -> originData.city = name
-                    "administrative_area_level_2" in types -> originData.province= name
-                    "administrative_area_level_1" in types -> originData.region = name
-                    "country" in types -> originData.country = name
-                    "postal_code" in types -> originData.postalCode = name
-                }
-
-                focusOriginBool=false // Eseguite tutte le operazioni toglie il focus
-            }
-
-
-        } else if (focusDestinationBool) {
-
-            destinationData.address=place.address
-
-            binding.editTextDestination.text=Editable.Factory.getInstance().newEditable(destinationData.address)
-
-            destinationData.latitude=place.latLng.latitude.toString()
-            destinationData.longitude=place.latLng.longitude.toString()
-
-            for (component in addressComponents) {
-                val types = component.types
-                val name = component.name
-
-                when {
-                    "locality" in types -> destinationData.city = name
-                    "administrative_area_level_2" in types -> destinationData.province= name
-                    "administrative_area_level_1" in types -> destinationData.region = name
-                    "country" in types -> destinationData.country = name
-                    "postal_code" in types -> destinationData.postalCode = name
-                }
-            }
-            focusDestinationBool=false // Eseguite tutte le operazioni toglie il focus
-        }
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(montedagoAncona, zoomLevel))
     }
 
-    private fun HttpRequestDirections() {
-        lifecycleScope.launch {
-            val origin = "${originData.latitude},${originData.longitude}"
-            val destination = "${destinationData.latitude},${destinationData.longitude}"
 
-            val url = URL("https://maps.googleapis.com/maps/api/directions/json" +
-                    "?origin=$origin" +
-                    "&destination=$destination" +
-                    "&mode=driving" +
-                    "&key=${getString(R.string.google_maps_api_key)}")
+    private fun drawRouteOnMap(jsonResponse: String) {
+        val decodedPath = mapViewModel.ParseRoute(jsonResponse)
 
-            withContext(Dispatchers.IO) {
-                (url.openConnection() as? HttpURLConnection)?.run {
-                    requestMethod = "GET"
-
-                    val responseCode = responseCode
-                    if (responseCode == HttpURLConnection.HTTP_OK) {
-                        val bufferedReader = BufferedReader(InputStreamReader(inputStream))
-                        val response = StringBuilder()
-
-                        var inputLine: String?
-                        while (bufferedReader.readLine().also { inputLine = it } != null) {
-                            response.append(inputLine)
-                        }
-                        bufferedReader.close()
-
-                        val jsonResponse = response.toString()
-                        Log.d("json", jsonResponse)
-                        Log.d("json", jsonResponse)
-
-
-                        val colorResource = R.color.app_theme
-                        val lineWidth = 20f // Larghezza della linea in pixel
-                        parseDistanceFromJson(jsonResponse)
-                        drawRouteOnMap(jsonResponse, colorResource, lineWidth)
-                    } else {
-                        // Gestisci qui gli errori di richiesta
-                    }
-
-                    disconnect()
-                }
-            }
-        }
-    }
-
-    private fun parseDistanceFromJson(jsonResponse: String): String {
-        val jsonObject = JSONObject(jsonResponse)
-        val routesArray = jsonObject.getJSONArray("routes")
-
-        if (routesArray.length() > 0) {
-            val route = routesArray.getJSONObject(0)
-            val legsArray = route.getJSONArray("legs")
-
-            if (legsArray.length() > 0) {
-                val leg = legsArray.getJSONObject(0)
-                val distanceObject = leg.getJSONObject("distance")
-                val distanceText = distanceObject.getString("text")
-
-                return distanceText
-            }
-        }
-
-        return "N/A" // Ritorna "N/A" se non è possibile estrarre la distanza
-    }
-
-    private fun drawRouteOnMap(jsonResponse: String, colorResource: Int, lineWidth: Float) {
         requireActivity().runOnUiThread {
-            val jsonObject = JSONObject(jsonResponse)
-            val routesArray = jsonObject.getJSONArray("routes")
 
-            if (routesArray.length() > 0) {
-                val route = routesArray.getJSONObject(0)
-                val overviewPolyline = route.getJSONObject("overview_polyline")
-                val points = overviewPolyline.getString("points")
+            val colorResource = R.color.app_theme
+            val lineWidth = 20f
 
-                val decodedPath = PolyUtil.decode(points) // Assicurati di avere importato PolyUtil
+
+            if (decodedPath.isNotEmpty()) {// Assicurati di avere importato PolyUtil
 
                 val color = ContextCompat.getColor(requireContext(), colorResource)
 
@@ -302,17 +228,27 @@ class HomeFragment : Fragment() , OnMapReadyCallback {
 
                 mMap.clear()
 
-                val startLatLng = LatLng(decodedPath.first().latitude, decodedPath.first().longitude)
-                val endLatLng = LatLng(decodedPath.last().latitude, decodedPath.last().longitude)
+                val startLatLng = LatLng(
+                    originData.latitude.toString().toDouble(),
+                    originData.longitude.toString().toDouble()
+                )
+                val endLatLng = LatLng(
+                    destinationData.latitude.toString().toDouble(),
+                    destinationData.longitude.toString().toDouble()
+                )
 
 
-                mMap.addMarker(MarkerOptions()
-                    .position(startLatLng)
-                    .title("Partenza"))
+                mMap.addMarker(
+                    MarkerOptions()
+                        .position(startLatLng)
+                        .title("Partenza")
+                )
 
-                mMap.addMarker(MarkerOptions()
-                    .position(endLatLng)
-                    .title("Arrivo"))
+                mMap.addMarker(
+                    MarkerOptions()
+                        .position(endLatLng)
+                        .title("Arrivo")
+                )
 
                 mMap.addPolyline(polylineOptions)
 
@@ -331,6 +267,5 @@ class HomeFragment : Fragment() , OnMapReadyCallback {
         }
     }
 
-
-
 }
+
--- app/src/main/java/com/example/easymove/ViewModel/MapViewModel.kt
+++ app/src/main/java/com/example/easymove/ViewModel/MapViewModel.kt
@@ -1,11 +1,162 @@
 package com.example.easymove.ViewModel
 
+import android.provider.Settings.Secure.getString
 import android.text.Editable
+import android.util.Log
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.lifecycleScope
+import com.example.easymove.R
 import com.example.easymove.model.MapData
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolylineOptions
 import com.google.android.libraries.places.api.model.Place
+import com.google.maps.android.PolyUtil
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import javax.security.auth.callback.Callback
 
 class MapViewModel: ViewModel() {
+    fun getAddressDetails(place: Place, originData: MapData, destinationData: MapData, focusOriginBool: Boolean){
 
+        // Estrae dalla variabile place restituita dall'autocomplete addressComponents
+        val addressComponents = place.addressComponents.asList()
+
+        // Se il focus è sull'Origin editext riempie i campi dell'oggetto originData altrimenti di destinationData
+        if(focusOriginBool){
+            originData.address =place.address
+
+
+
+            originData.latitude=place.latLng.latitude.toString()
+            originData.longitude=place.latLng.longitude.toString()
+
+            for (component in addressComponents) {
+                val types = component.types
+                val name = component.name
+
+                when {
+                    "locality" in types -> originData.city = name
+                    "administrative_area_level_2" in types -> originData.province= name
+                    "administrative_area_level_1" in types -> originData.region = name
+                    "country" in types -> originData.country = name
+                    "postal_code" in types -> originData.postalCode = name
+                }
+
+            }
+
+
+        } else  {
+
+            destinationData.address=place.address
+
+
+
+            destinationData.latitude=place.latLng.latitude.toString()
+            destinationData.longitude=place.latLng.longitude.toString()
+
+            for (component in addressComponents) {
+                val types = component.types
+                val name = component.name
+
+                when {
+                    "locality" in types -> destinationData.city = name
+                    "administrative_area_level_2" in types -> destinationData.province= name
+                    "administrative_area_level_1" in types -> destinationData.region = name
+                    "country" in types -> destinationData.country = name
+                    "postal_code" in types -> destinationData.postalCode = name
+                }
+            }
+        }
+    }
+
+    fun HttpRequestDirections(viewModelScope: CoroutineScope, originData: MapData, destinationData: MapData, apiKey: String, callback: (String)-> Unit) {
+        viewModelScope.launch {
+            val origin = "${originData.latitude},${originData.longitude}"
+            val destination = "${destinationData.latitude},${destinationData.longitude}"
+
+            val url = URL("https://maps.googleapis.com/maps/api/directions/json" +
+                    "?origin=$origin" +
+                    "&destination=$destination" +
+                    "&mode=driving" +
+                    "&key=${apiKey}")
+
+            withContext(Dispatchers.IO) {
+                (url.openConnection() as? HttpURLConnection)?.run {
+                    requestMethod = "GET"
+
+                    val responseCode = responseCode
+                    if (responseCode == HttpURLConnection.HTTP_OK) {
+                        val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+                        val response = StringBuilder()
+
+                        var inputLine: String?
+                        while (bufferedReader.readLine().also { inputLine = it } != null) {
+                            response.append(inputLine)
+                        }
+                        bufferedReader.close()
+
+                        val jsonResponse = response.toString()
+                        Log.d("json", jsonResponse)
+                        Log.d("json", jsonResponse)
+
+                        callback(jsonResponse)
+                    } else {
+                        // Gestisci qui gli errori di richiesta
+                    }
+
+                    disconnect()
+                }
+            }
+        }
+    }
+
+    fun parseDistanceTimeFromJson(jsonResponse: String): Pair<String, String> {
+        val jsonObject = JSONObject(jsonResponse)
+        val routesArray = jsonObject.getJSONArray("routes")
+
+        if (routesArray.length() > 0) {
+            val route = routesArray.getJSONObject(0)
+            val legsArray = route.getJSONArray("legs")
+
+            if (legsArray.length() > 0) {
+                val leg = legsArray.getJSONObject(0)
+                val distanceObject = leg.getJSONObject("distance")
+                val distanceText = distanceObject.getString("text")
+
+                val durationObject = leg.getJSONObject("duration")
+                val durationText = durationObject.getString("text")
+
+                return Pair(distanceText, durationText)
+            }
+        }
+
+        return Pair("N/A","N/A") // Ritorna "N/A" se non è possibile estrarre la distanza
+    }
+
+ fun ParseRoute(jsonResponse: String): MutableList<LatLng> {
+     val jsonObject = JSONObject(jsonResponse)
+     val routesArray = jsonObject.getJSONArray("routes")
+     if (routesArray.length() > 0) {
+         val route = routesArray.getJSONObject(0)
+         val overviewPolyline = route.getJSONObject("overview_polyline")
+         val points = overviewPolyline.getString("points")
+
+         return PolyUtil.decode(points)
+     }
+  return mutableListOf()
+ }
 
 }
