015635d31497283ab3ee16a23145f82dae32c8b2
DavideTicchiarelli <s1098361@studenti.univpm.it>
2023-08-30 20:06:13 +0200

    hide keyboard in mainfragment

--- app/src/main/java/com/example/easymove/View/MainFragment.kt
+++ app/src/main/java/com/example/easymove/View/MainFragment.kt
@@ -1,11 +1,13 @@
 package com.example.easymove.View
 
+import android.content.Context
 import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.annotation.RequiresApi
 import androidx.lifecycle.ViewModelProvider
 import com.example.easymove.R
@@ -40,6 +42,8 @@ class MainFragment : Fragment() {
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        hideKeyboard()
 
         homeViewModel = HomeViewModel()
         userViewModel = ViewModelProvider(requireActivity()).get(UserViewModel::class.java)
@@ -104,6 +108,9 @@ class MainFragment : Fragment() {
             .addToBackStack(null)
             .commit()
     }
-
+    private fun hideKeyboard() {
+        val imm = requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view?.windowToken, 0)
     }
+}
 
