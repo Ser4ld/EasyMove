f3cfc8ef0ee2afa36fcd4292291347499e5b2311
DavideTicchiarelli <s1098361@studenti.univpm.it>
2023-08-31 13:56:35 +0200

    fragment VeicoliGuidatorePublicFragment.kt

---
+++ app/src/main/java/com/example/easymove/View/VeicoliGuidatorePublicFragment.kt
@@ -0,0 +1,88 @@
+package com.example.easymove.View
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.easymove.ViewModel.UserViewModel
+import com.example.easymove.ViewModel.VeicoliViewModel
+import com.example.easymove.adapter.MyAdapterVeicoli
+import com.example.easymove.databinding.FragmentVeicoliGuidatorePublicBinding
+import com.example.easymove.model.User
+
+class VeicoliGuidatorePublicFragment : Fragment() {
+
+    private var _binding: FragmentVeicoliGuidatorePublicBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var userViewModel: UserViewModel
+    private lateinit var veicoliViewModel: VeicoliViewModel
+    private lateinit var user: User
+    private lateinit var adapter: MyAdapterVeicoli
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentVeicoliGuidatorePublicBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        userViewModel = ViewModelProvider(requireActivity()).get(UserViewModel::class.java)
+        veicoliViewModel = ViewModelProvider(requireActivity()).get(VeicoliViewModel::class.java)
+
+        val layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerView.layoutManager = layoutManager
+
+        binding.floatingActionButton2.setOnClickListener {
+            parentFragmentManager.popBackStack()
+        }
+
+        userViewModel.allUsersLiveData.observe(viewLifecycleOwner) { userList ->
+            userViewModel.userDataLiveData.observe(viewLifecycleOwner) { userData ->
+                if (userData != null && userList != null) {
+                    val argument = arguments
+                    if (argument != null) {
+                        val guidatoreId = argument.getString("idguidatore").toString()
+                        user = userViewModel.FilterListById(guidatoreId, userList)!!
+
+                        if (user != null) {
+                            adapter = MyAdapterVeicoli(
+                                veicoliViewModel,
+                                userViewModel,
+                                ArrayList(),
+                                "",
+                                userList
+                            )
+                            binding.recyclerView.adapter = adapter
+
+                            veicoliViewModel.veicoliLiveData.observe(viewLifecycleOwner) { veicoliList ->
+                                val recensioniFiltrate =
+                                    veicoliViewModel.filterVeicoliByUserId(guidatoreId, veicoliList)
+
+                                if (recensioniFiltrate.isEmpty()) {
+                                    binding.emptyLayout.visibility = View.VISIBLE
+                                } else {
+                                    binding.emptyLayout.visibility = View.GONE
+                                }
+
+                                adapter.updateData(recensioniFiltrate)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
